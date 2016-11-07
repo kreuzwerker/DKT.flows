6c4194c490fc680d8ae400fd0746657e9f8f1b2b
Alex Wilde <alexosoft@gmail.com>
2016-11-07 18:37:52 +0100

    #5 installed ng2-material.

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -3,11 +3,11 @@ import { UserService } from './user/user.service';
 
 // --- DKT vendors ---------------------------------------
 
-// import {MATERIAL_BROWSER_PROVIDERS} from "ng2-material";
+import {MATERIAL_BROWSER_PROVIDERS} from "ng2-material";
 
 export const APP_PROVIDERS = [
   UserActions,
   UserService,
 
-  // MATERIAL_BROWSER_PROVIDERS
+  MATERIAL_BROWSER_PROVIDERS
 ];
