676ad6c930242ad18b5693802252187bdb3a5cda
Alex Wilde <alexosoft@gmail.com>
2017-01-17 19:08:57 +0100

    #14 automatic store updates after mutations (eventual consistency).

--- src/app/apollo-client.ts
+++ src/app/apollo-client.ts
@@ -5,6 +5,10 @@ const client = new ApolloClient({
     // DKT Mock API
     uri: 'https://api.graph.cool/simple/v1/ciy0ib4ja13ay01441ubnmo7z'
   }),
+  // ID mapping required for automatic updates of objects in the store after
+  // mutations.
+  dataIdFromObject: o => o['id'],
+  // Enable Apollo Dev Tools Extension
   connectToDevTools: true
 });
 
