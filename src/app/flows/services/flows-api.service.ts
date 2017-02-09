3716f6c90fa456b9757b2323321009983ce49385
Alex Wilde <alexosoft@gmail.com>
2017-02-09 18:27:16 +0100

    Fix for step mutation.

--- src/app/flows/services/flows-api.service.ts
+++ src/app/flows/services/flows-api.service.ts
@@ -52,7 +52,7 @@ export class FlowsApiService {
       variables: {
         id: id,
         position: position,
-        serviceId: serviceId
+        service: serviceId
       }
     }).map(({data}) => data.updateStep);
   }
