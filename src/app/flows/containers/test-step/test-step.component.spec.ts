4bf75c276c605666bd988ec674fef0a270df6752
Alex Wilde <alexosoft@gmail.com>
2017-02-13 22:19:38 +0100

    #20 implemented TestStep Component tests.

--- src/app/flows/containers/test-step/test-step.component.spec.ts
+++ src/app/flows/containers/test-step/test-step.component.spec.ts
@@ -1,9 +1,25 @@
 /* tslint:disable: ter-max-len */
+import { mockFlowsApp } from './../../utils/test.helpers';
+import { TestStepComponent } from './test-step.component';
+import { FlowsAppService } from './../../services';
+
 describe('Flows App', () => {
 
   describe('TestStep Component', () => {
+    let component: TestStepComponent;
+    let flowsApp: FlowsAppService;
+
+    beforeEach(() => {
+      flowsApp = mockFlowsApp;
+      component = new TestStepComponent(flowsApp);
+      expect(component).toBeTruthy();
+    });
+
     describe('ngOnInit()', () => {
-      xit('should set current step preparation stage to "test"', () => {
+      it('should set current step preparation stage to "test"', () => {
+        spyOn(flowsApp, 'setStepStage');
+        component.ngOnInit();
+        expect(flowsApp.setStepStage).toHaveBeenCalledWith('test');
       });
     });
   });
