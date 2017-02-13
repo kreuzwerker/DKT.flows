4030fd1bd750b6bc4d9069caa5fc7d1022dbf119
Alex Wilde <alexosoft@gmail.com>
2017-02-13 21:41:38 +0100

    #20 implemented ConfigureStep Component tests.

--- src/app/flows/containers/configure-step/configure-step.component.spec.ts
+++ src/app/flows/containers/configure-step/configure-step.component.spec.ts
@@ -1,9 +1,29 @@
 /* tslint:disable: ter-max-len */
+import { TestUtils } from './../../utils/test.helpers';
+import { ConfigureStepComponent } from './configure-step.component';
+import { FlowsAppService } from './../../services';
+
 describe('Flows App', () => {
 
   describe('ConfigureStep Component', () => {
+    let component: ConfigureStepComponent;
+    let utils: TestUtils;
+    let flowsApp: FlowsAppService;
+
+    beforeEach(() => {
+      utils = new TestUtils();
+      flowsApp = {
+        setStepStage(stage: string): void { }
+      } as FlowsAppService
+      component = new ConfigureStepComponent(flowsApp);
+      expect(component).toBeTruthy();
+    });
+
     describe('ngOnInit()', () => {
-      xit('should set current step preparation stage to "configure"', () => {
+      it('should set current step preparation stage to "configure"', () => {
+        spyOn(flowsApp, 'setStepStage');
+        component.ngOnInit();
+        expect(flowsApp.setStepStage).toHaveBeenCalledWith('configure');
       });
     });
   });
