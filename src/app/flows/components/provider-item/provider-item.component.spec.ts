1400e040fb4c4cc4c9f416a992b0e54220f5317b
Alex Wilde <alexosoft@gmail.com>
2017-02-13 21:23:10 +0100

    #20 implemented ProviderDetail Component tests.

--- src/app/flows/components/provider-item/provider-item.component.spec.ts
+++ src/app/flows/components/provider-item/provider-item.component.spec.ts
@@ -1,13 +1,30 @@
 /* tslint:disable: ter-max-len */
+import { Provider } from './../../models';
+import { TestUtils } from './../../utils/test.helpers';
+import { ProviderItemComponent } from './provider-item.component';
+
 describe('Flows App', () => {
   describe('ProviderDetail Component', () => {
+    let component: ProviderItemComponent;
+    let utils: TestUtils;
+
+    beforeEach(() => {
+      utils = new TestUtils();
+      component = new ProviderItemComponent();
+      expect(component).toBeTruthy();
+    });
 
     // UI
     // - should display item as selected if is selected
     // - I can select the step
 
     describe('select()', () => {
-      xit('should emit an onSelectProvider event with the correct params', () => {
+      it('should emit an onSelectProvider event with the correct params', () => {
+        spyOn(component.onSelectProvider, 'emit');
+        const provider: Provider = utils.createProviderData();
+        component.provider = provider;
+        component.select();
+        expect(component.onSelectProvider.emit).toHaveBeenCalledWith({provider: provider});
       });
     });
   });
