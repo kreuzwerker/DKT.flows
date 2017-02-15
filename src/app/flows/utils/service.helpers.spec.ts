3e7ea21d2e77e53a122701d768c76887d96f88b5
Alex Wilde <alexosoft@gmail.com>
2017-02-15 12:19:26 +0100

    #20 implemented Provider Service Helpers tests.

--- src/app/flows/utils/service.helpers.spec.ts
+++ src/app/flows/utils/service.helpers.spec.ts
@@ -1,9 +1,25 @@
 /* tslint:disable: ter-max-len */
+import { TestUtils } from './test.helpers';
+import * as helpers from './service.helpers';
+import { Service, ServiceType } from './../models';
+
 describe('Flows App', () => {
 
   describe('Provider Service Helpers', () => {
+    let service: Service;
+    let utils: TestUtils;
+
+    beforeEach(() => {
+      utils = new TestUtils();
+      service = utils.createServiceData();
+    });
+
     describe('getServiceTypeName()', () => {
-      xit('should return the correct service type for the given service', () => {
+      it('should return the correct service type for the given service', () => {
+        service.type = ServiceType.TRIGGER;
+        expect(helpers.getServiceTypeName(service)).toBe('Trigger');
+        service.type = ServiceType.ACTION;
+        expect(helpers.getServiceTypeName(service)).toBe('Action');
       });
     });
   });
