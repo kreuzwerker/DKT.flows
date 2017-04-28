59e8b3a89c3c1dd5c8e00c5e8afe19a8a3d1e9c7
Anthony <qdouble@gmail.com>
2017-04-28 03:28:14 -0700

    Update Universal Branch to angular 4.1 (#273)
    * update packages...WIP
    
    * initial setup
    
    * tsconfig cleanup
    
    * aot config WIP
    
    * fix server:aot and add analyzer constant
    
    * enable mock-api
    
    * cleanup

--- constants.d.ts
+++ constants.d.ts
@@ -3,6 +3,8 @@ export declare const DEV_PORT: number;
 export declare const E2E_PORT: number;
 export declare const PROD_PORT: number;
 
+export declare const SHOW_WEBPACK_BUNDLE_ANALYZER: boolean;
+
 export declare const USE_DEV_SERVER_PROXY: boolean;
 export declare const DEV_SERVER_PROXY_CONFIG: any;
 
