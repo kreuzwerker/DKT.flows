08aa7ba20b6365cc58c04356e6e05937009d2529
Alex Wilde <alexosoft@gmail.com>
2017-05-23 11:00:59 +0200

    Stretch layout to 100% and grow beyond.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserLoginService } from './core/services';
 
-import { views } from './app-nav-views';
-import { MOBILE } from './services/constants';
-
 @Component({
-  selector: 'my-app',
+  selector: 'dkt-app',
   styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None
@@ -15,13 +12,6 @@ export class AppComponent {
   showMonitor = (ENV === 'development' && !AOT &&
     ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
   );
-  mobile = MOBILE;
-
-  // Hide sidebar by default
-  showSidebar = false;
-
-  sideNavMode = MOBILE ? 'over' : 'side';
-  views = views;
 
   constructor(
     public route: ActivatedRoute,
