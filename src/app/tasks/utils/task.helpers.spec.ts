eb70547fc8058dec1a6326defa6f780bd4a76dfa
Alex Wilde <alexosoft@gmail.com>
2017-06-21 12:34:37 +0200

    #37 added tests.

--- src/app/tasks/utils/task.helpers.spec.ts
+++ src/app/tasks/utils/task.helpers.spec.ts
@@ -1,8 +1,31 @@
 /* tslint:disable: ter-max-len */
+import { TestUtils } from './test.helpers';
+import * as helpers from './task.helpers';
+import { Task, TaskType, TaskStateType } from './../models';
 
 describe('Tasks App', () => {
 
   describe('Task Helpers', () => {
+    let task: Task;
+    let utils: TestUtils;
 
+    beforeEach(() => {
+      utils = new TestUtils();
+      task = utils.createTaskData();
+    });
+
+    describe('getTaskStateType()', () => {
+      it('should return the task state type for the given task', () => {
+        task.type = TaskType.APPROVE;
+        let type = helpers.getTaskStateType(task);
+        expect(type).toBe(TaskStateType.APPROVE);
+        task.type = TaskType.REVIEW;
+        type = helpers.getTaskStateType(task);
+        expect(type).toBe(TaskStateType.REVIEW);
+        task.type = TaskType.CORRECT;
+        type = helpers.getTaskStateType(task);
+        expect(type).toBe(TaskStateType.TRANSITIONAL);
+      });
+    });
   });
 });
