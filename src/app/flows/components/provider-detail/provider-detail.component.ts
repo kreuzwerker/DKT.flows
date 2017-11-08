85863854a9e2eb4b7f4f42283d281aa5c6a5d410
Alex Wilde <alexosoft@gmail.com>
2017-11-08 09:41:24 +0700

    Made providers list and provider detail dialog responsive.

--- src/app/flows/components/provider-detail/provider-detail.component.ts
+++ src/app/flows/components/provider-detail/provider-detail.component.ts
@@ -11,8 +11,8 @@ import * as providerHelpers from './../../utils/provider.helpers';
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger('fadeInOut', [
-      state('showing' , style({ opacity: 1, transform: 'scale(1.0)' })),
-      state('hiding', style({ opacity: 0, transform: 'scale(0.0)' })),
+      state('showing' , style({ opacity: 1, transform: 'scale(1.0) translateX(-50%)' })),
+      state('hiding', style({ opacity: 0, transform: 'scale(0.0) translateX(-50%)' })),
       transition('* <=> *', animate('200ms')),
     ])
   ]
