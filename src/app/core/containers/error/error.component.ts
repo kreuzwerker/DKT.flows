c054df645735e57a675999698eb92c139823d28c
Alex Wilde <alexosoft@gmail.com>
2017-12-01 10:33:35 +0100

    #63 Extended error messages with account entity.

--- src/app/core/containers/error/error.component.ts
+++ src/app/core/containers/error/error.component.ts
@@ -8,29 +8,44 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ErrorComponent {
   entities = {
-    'task': {
-      sing: 'task', plur: 'tasks', capSing: 'Task', capPlur: 'Tasks', returnRoute: 'tasks'
+    account: {
+      sing: 'account',
+      plur: 'accounts',
+      capSing: 'Account',
+      capPlur: 'Accounts',
+      returnRoute: 'accounts'
     },
-    'flow': {
-      sing: 'flow', plur: 'flows', capSing: 'Flow', capPlur: 'Flows', returnRoute: 'flows'
+    task: {
+      sing: 'task',
+      plur: 'tasks',
+      capSing: 'Task',
+      capPlur: 'Tasks',
+      returnRoute: 'tasks'
+    },
+    flow: {
+      sing: 'flow',
+      plur: 'flows',
+      capSing: 'Flow',
+      capPlur: 'Flows',
+      returnRoute: 'flows'
     }
   };
 
   code: string;
   entityType: string;
 
-  constructor(
-    public route: ActivatedRoute,
-    public router: Router
-  ) {
+  constructor(public route: ActivatedRoute, public router: Router) {
     this.code = this.route.snapshot.params.code;
     this.entityType = this.route.snapshot.params.entity;
   }
 
   message() {
+    console.log('this.entityType', this.entityType);
     switch (parseInt(this.code, 10)) {
       case 401:
-        return `You don't have permission to access this ${this.entities[this.entityType].sing}`;
+        return `You don't have permission to access this ${
+          this.entities[this.entityType].sing
+        }`;
 
       case 404:
         return `${this.entities[this.entityType].capSing} not found`;
