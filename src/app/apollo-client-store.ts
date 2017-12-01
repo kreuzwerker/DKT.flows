ebe075588f8e957429973fec564315f4e83d0b8c
Alex Wilde <alexosoft@gmail.com>
2017-12-01 11:07:43 +0100

    Map flows.kreuzwerker.net hostname to Test endpoint.

--- src/app/apollo-client-store.ts
+++ src/app/apollo-client-store.ts
@@ -3,7 +3,10 @@ import { UserLoginService } from './core/services';
 import { each } from 'lodash';
 
 function getApiUrl(): string {
-  if (document.location.hostname.indexOf('dkt.flows.test') !== -1) {
+  if (
+    document.location.hostname.indexOf('flows.kreuzwerker.net') !== -1 ||
+    document.location.hostname.indexOf('dkt.flows.test') !== -1
+  ) {
     return 'https://x64ywwtnw3.execute-api.eu-west-1.amazonaws.com/Test';
   } else {
     return 'https://m0zw22d92f.execute-api.eu-west-1.amazonaws.com/Dev';
